ac03a6d444a757ff4670445592cffce87ae98d1e
Gaetan BERNHARD <gaetan.bernhard@gmail.com>
2020-07-11 19:53:50 +0000

    Seeder for packages table

---
+++ database/seeds/PackageSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+/**
+ * Seeder to fill the packages table
+ */
+class PackageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds to fill the packages table
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $packageQty = 15;
+        $lengthPackageName = 10;
+
+        for ($index = 1 ; $index <= $packageQty ; $index++) {
+            DB::table('packages')->insert([
+                'name' => strtolower('Package N°' . $index . ': ' . Str::random($lengthPackageName)),
+                'limit' => random_int(3,8)
+            ]);
+        }
+
+    }
+}
